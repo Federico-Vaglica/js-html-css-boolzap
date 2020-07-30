42e6b696e606555045acee2bbafb7febbe40d0ed
FedericoV <vaglicafederico95@gmail.com>
2020-07-30 14:20:02 +0200

    Ottimizzazione layout

--- js/script.js
+++ js/script.js
@@ -16,9 +16,10 @@ $(document).ready(function(){
 
         setTimeout(myFunction, 3000);
         function myFunction() {
-            var clone = $('.template1 .container-msg').clone();
-            
-            $('.container-chat').append(clone);
+            var clone = $('.template .container-msg').clone();
+            clone.find('.messaggio').addClass('fleft ricevuto');
+            clone.find('.clone-msg').text('OK');
+            $('.right-conversation.active').append(clone);
     }
     })
 
@@ -26,11 +27,11 @@ $(document).ready(function(){
     function invioMessaggio() {
         var valoreInput = $('#myMsg').val();
                 var clone = $('.template .container-msg').clone();
-                var x = clone.children('.messaggio');
-                x.children('.clone-msg').text(valoreInput);
+                clone.find('.messaggio').addClass('fright inviato');
+                clone.find('.clone-msg').text(valoreInput);
                 console.log(clone.children('.clone-msg'));
                 
-                $('.container-chat').append(clone);    
+                $('.right-conversation.active').append(clone);    
     }
 });
 
